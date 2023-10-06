50fa4a17ba414316fec27fe224931ef4d535968e
pallavee-2705 <pallaveebisoyee@gmail.com>
2023-10-05 22:25:46 -0400

    added repo link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/pallavee-2705/ai_summarizer.git", "_blank")
           }
           className='black_btn'
         >
